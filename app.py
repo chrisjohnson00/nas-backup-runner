c3390f54f4f3e3526f1278aa858eeafe2316e314
Chris Johnson <chrisjohnson00@hotmail.com>
2021-06-25 17:45:23 -0700

    feat: initial version

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ import subprocess
 
 def main():
     rsyncs = [
-        'rsync -a --progress /data/files/ chris@192.168.1.132:/data/files/ >> /var/log/backup_files.log 2>&1',  # noqa: E501
-        'rsync -ar --ignore-errors --progress --delete-before /data/video/ chris@192.168.1.132:/data/video/ >> /var/log/backup_videos.log 2>&1',  # noqa: E501
-        'rsync -ar --ignore-errors --progress --delete-before /data/k8s/ chris@192.168.1.132:/data/k8s/ >> /var/log/backup_k8s.log 2>&1',  # noqa: E501
-        'rsync -a --progress --delete-before /docker/ chris@192.168.1.132:/data/docker/ >> /var/log/backup_docker.log 2>&1',  # noqa: E501
-        'rsync -a --progress --delete-before --delete-before /data/smb/ chris@192.168.1.132:/data/smb/ >> /var/log/backup_smb.log 2>&1'  # noqa: E501
+        'rsync -a --progress /data/files/ chris@192.168.1.132:/data/files/',
+        'rsync -ar --ignore-errors --progress --delete-before /data/video/ chris@192.168.1.132:/data/video/',
+        'rsync -ar --ignore-errors --progress --delete-before /data/k8s/ chris@192.168.1.132:/data/k8s/',
+        'rsync -a --progress --delete-before /docker/ chris@192.168.1.132:/data/docker/',
+        'rsync -a --progress --delete-before --delete-before /data/smb/ chris@192.168.1.132:/data/smb/'
     ]
     for rsync in rsyncs:
         logging.info(f"Running: {rsync}")
-        subprocess.run(rsync, check=True)
+        subprocess_completed = subprocess.run(rsync, check=True, capture_output=True, text=True)
+        logging.info(f"{subprocess_completed.stdout}")
+        logging.error(f"{subprocess_completed.stderr}")
         logging.info(f"Completed: {rsync}")
 
 
